30d1927235dc46b9276f5371b4513ea46582e336
keshavsingh001k <keshavsingh123007@gmail.com>
2026-06-15 15:58:49 +0530

    Upload day_14

---
+++ day_14/q55.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    int largest=-99999, second=-99999;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            second=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]>second && arr[i]!=largest){
+            second=arr[i];
+        }
+    }
+
+    cout<<"Second Largest = "<<second;
+
+    return 0;
+}
