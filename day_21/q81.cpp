3d5039b3f5bc6be118d95448df0b5a87bfff3fd6
keshavsingh001k <keshavsingh123007@gmail.com>
2026-06-21 17:26:02 +0530

    day_21 files

---
+++ day_21/q81.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cin.getline(str, 100);
+
+    int len = 0;
+
+    while(str[len] != '\0')
+        len++;
+
+    cout << len;
+
+    return 0;
+}
